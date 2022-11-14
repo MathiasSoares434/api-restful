29600eb7c230f9de33652f2e3f5879c900909cc1
Mathias Soares <mathiasernandes02@hotmail.com>
2022-11-13 23:03:55 -0300

    acrescentando metodo get

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,7 +1,12 @@
 const ProductsModel = require('../models/products')
 
 async function get(req, res){
-    const products = await ProductsModel.find()
+    const { id } = req.params /// ?id=123
+    
+// parte mais importante da aula, saber como fazer o postman ler os id's sem precisar indenfica-los
+    const obj = id ? { _id: id } : null //if ternário
+
+    const products = await ProductsModel.find(obj)
 
 
     res.send(products)
